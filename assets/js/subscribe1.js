9ab7b91c68230f953f867a303766ca50adb1e7cc
JennyKim <jennytkim@gmail.com>
2018-09-25 22:40:27 -0400

    changes

--- assets/js/subscribe1.js
+++ assets/js/subscribe1.js
@@ -1,8 +1,5 @@
 var data = JSON.stringify($(".homesubscription"));
   
-  var xhr = new XMLHttpRequest();
-  xhr.withCredentials = true;
-  
   xhr.addEventListener("readystatechange", function () {
     if (this.readyState === this.DONE) {
       console.log(this.responseText);
